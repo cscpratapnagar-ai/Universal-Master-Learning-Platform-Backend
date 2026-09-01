282ec674ee8e02fc53b09494caba5dab3b331e51
cscpratapnagar-ai <cscpratapnagar@gmail.com>
2026-09-01 16:20:03 +0530

    feat: add JWT bearer authentication to Swagger OpenAPI

---
+++ src/main/java/com/masterlearning/platform/config/OpenApiConfig.java
@@ -0,0 +1,33 @@
+package com.masterlearning.platform.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
+    @Bean
+    public OpenAPI masterLearningOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Universal Master Learning Platform API")
+                        .version("v1")
+                        .description("API documentation for Universal Master Learning Platform"))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(
+                                SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        ));
+    }
+}
